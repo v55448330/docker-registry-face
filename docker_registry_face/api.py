5122f8c13e527370a27c85a09ca6731ef5caf490
XiaoBao Zhang <dyzhang@XiaoBao.local>
2017-03-31 18:51:14 +0800

    update del image

--- docker_registry_face/api.py
+++ docker_registry_face/api.py
@@ -1,9 +1,10 @@
 from docker_registry_face import app
-from flask import render_template, request
+from flask import render_template, request, make_response
 from requests.auth import HTTPBasicAuth
 from config import reload_config
 import requests
 import json
+import time
 
 @app.route('/api/v1/image', methods=['GET'])
 def get_images():
@@ -62,10 +63,9 @@ def get_image_tags():
     image = request.args.get('image','')
     tags = {
         "image_name":image,
-        "tag_list":{}
+        "tag_list":[]
     }
     try:
-        img = {}
         config = reload_config() 
         registry_url = config.get('registry_url')
         registry_user = config.get('registry_user')
@@ -78,18 +78,19 @@ def get_image_tags():
             if r.status_code == 200:
                 t_list = json.loads(r.text).get('tags',[])
                 for t in t_list:
+                    img = {}
                     tr = requests.get(url=registry_url + "/v2/" + image + "/manifests/" + t, 
                         auth=HTTPBasicAuth(registry_user,registry_password), 
                         timeout=5,
                         headers={'Accept':'application/vnd.docker.distribution.manifest.v2+json'}
                     )
-                    print tr.headers
                     t_info = json.loads(tr.text)
-                    img["layer_count"] = [len(t_info.get('layers'))]
-                    img["layer_detail"] = t_info.get('layers')
-                    img["url"] = registry_url + "/" + image + ":" + t
-                    img["tag"] = t
-                    tags['tag_list'][t] = img
+                    if not t_info.has_key('errors'):
+                        img["layer_count"] = len(t_info.get('layers'))
+                        img["layer_detail"] = t_info.get('layers')
+                        img["url"] = registry_url + "/" + image + ":" + t
+                        img["tag"] = t
+                        tags['tag_list'].append(img)
         # print json.dumps(tags)
     except Exception,e:
         print "get tags error: " + str(e)
@@ -130,3 +131,67 @@ def get_tag_history():
     except Exception,e:
         print "get history error: " + str(e)
     return json.dumps(history)
+
+@app.route('/api/v1/tag', methods=['DELETE'])
+def del_image_tag():
+    config = reload_config() 
+    registry_url = config.get('registry_url')
+    registry_user = config.get('registry_user')
+    registry_password = config.get('registry_password')
+    result = {"result":""}
+    status = 400
+    try:
+        req = request.json
+        image = req.get('image','')
+        tag = req.get('tag','')
+        if image \
+            and tag:
+            r = requests.get(url=registry_url + "/v2/" + image + "/manifests/" + tag, 
+                auth=HTTPBasicAuth(registry_user,registry_password), 
+                headers={'Accept':'application/vnd.docker.distribution.manifest.v2+json'},
+                timeout=5
+            )
+            digest = r.headers.get('Docker-Content-Digest','')
+            print digest
+            if digest:
+                dr = del_manifests(image,digest)
+                if dr == 202 and get_manifests(image,tag):
+                    status = 200
+                else:
+                    dr = del_manifests(image,digest)
+                    if dr == 202 and get_manifests(image,tag):
+                        status = 200
+
+    except Exception,e:
+        print "del image error: " + str(e)
+        result["result"] = str(e)
+    return make_response(json.dumps(result),status)
+
+def del_manifests(image,digest):
+    config = reload_config() 
+    registry_url = config.get('registry_url')
+    registry_user = config.get('registry_user')
+    registry_password = config.get('registry_password')
+
+    del_url = registry_url + "/v2/" + image + "/manifests/" + digest
+    r = requests.delete(url=del_url, 
+        auth=HTTPBasicAuth(registry_user,registry_password), 
+        timeout=5
+    )
+    return r.status_code
+
+def get_manifests(image,tag):
+    result = True
+    config = reload_config() 
+    registry_url = config.get('registry_url')
+    registry_user = config.get('registry_user')
+    registry_password = config.get('registry_password')
+
+    r = requests.get(url=registry_url + "/v2/" + image + "/manifests/" + tag, 
+        auth=HTTPBasicAuth(registry_user,registry_password), 
+        timeout=5
+    )
+    if not json.loads(r.text).has_key('errors'):
+        result = False
+
+    return result
